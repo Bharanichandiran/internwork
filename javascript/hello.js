eafde56fb8f66d79445194f51ee43e4f4bc43e52
MCA <bharanichandhran@gmail.com>
2025-01-02 15:46:30 +0530

    day 13

---
+++ javascript/hello.js
@@ -0,0 +1,24 @@
+console.log("hello");
+var a=10, b=20,c=30;
+if(a<b&&b<c){
+    console.log("a is smaller than b and b is smaller than c");
+}
+else if(a<b||b<c){
+    console.log("hello")
+}
+else{
+    console.log("a is not smaller than b and b is not smaller than c");
+}
+let i=1;
+while(i<=5){
+    console.log("Bharani");
+    i++;
+}
+for(j=1;j<=5;j++){
+    console.log("Chandran");
+}
+function add(a,b){
+    console.log( a+b);
+    //return a+b;}
+    add(9,1);
+}
